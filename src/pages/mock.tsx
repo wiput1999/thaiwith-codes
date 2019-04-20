6f4e93c7bca58b05b0c793119ee975f3a2a07c3a
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-20 15:47:43 +0700

    :sparkles: Init mock page

---
+++ src/pages/mock.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {Box, Card, Image, Heading, Text, Flex, Button, Link} from 'rebass'
+
+export default class MockPage extends React.Component {
+  public render() {
+    return (
+      <div>
+        <div>
+          Nav
+        </div>
+        <div>
+          <div>Featured</div>
+          <div>More</div>
+        </div>
+        <div>
+          Footer
+        </div>
+      </div>
+    )
+  }
+}
