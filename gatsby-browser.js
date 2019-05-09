b5117958bb934761efa90c2db2bb0db7a2b36950
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-10 01:40:43 +0700

    :ambulance: cache

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 
 import { App } from './src/components/app'
 
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload()
+}
+
 export const wrapPageElement = ({ element }) => <App>{element}</App>
 
 wrapPageElement.propTypes = {
