0ee21967e232277d2f528d6d7a3fd83f6d8baf90
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-10 12:26:43 +0700

    :art: Styled 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,6 +4,8 @@ import Helmet from 'react-helmet'
 
 import { graphql } from 'gatsby'
 
+import { Box, Flex, Heading } from 'rebass'
+
 interface NotFoundPageProp {
   data: {
     site: {
@@ -25,8 +27,12 @@ class NotFoundPage extends React.Component<NotFoundPageProp> {
     return (
       <>
         <Helmet title={`Not Found`} />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Flex alignItems='center'>
+          <Box mt={50} mb={50} mx='auto' width={[22 / 24, 22 / 24, 22 / 24, 20 / 24]}>
+            <Heading mb={4}>NOT FOUND</Heading>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </Box>
+        </Flex>
       </>
     )
   }
