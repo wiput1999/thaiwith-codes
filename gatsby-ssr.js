02c30adfc09ba07073b12dfa6548a3e505592c6f
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-11 04:20:18 +0700

    :sparkles: Add gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types'
 
 import { App } from './src/components/app'
 
-export const onServiceWorkerUpdateReady = () => {
-  window.location.reload()
-}
-
 export const wrapPageElement = ({ element, props }) => <App {...props}>{element}</App>
 
 wrapPageElement.propTypes = {
