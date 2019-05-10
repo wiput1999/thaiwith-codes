3d2b6b9d8ea1820bf7ec99efa7af9635a873a832
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-10 23:53:31 +0700

    :sparkles: CHANGE TO MT

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -9,7 +9,7 @@ interface BlogCardInterface {
 export function Title({ title }: BlogCardInterface) {
   return (
     <Flex alignItems='center'>
-      <Box mx='auto' width={20 / 24} style={{marginTop: '50px'}}>
+      <Box mx='auto' width={20 / 24} mt={50}>
         <Heading mb={4}>{title}</Heading>
       </Box>
     </Flex>
